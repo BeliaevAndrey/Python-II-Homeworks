f8745d9f9d167cf9ba8636ac899124ab7462870f
Andrei Beliaev <abbot190414@gmail.com>
2023-03-07 23:16:33 +0300

    HW01: Task-03 fixed

--- homework_01/task03.py
+++ homework_01/task03.py
@@ -4,10 +4,10 @@
 # отрицательных чисел и чисел больше 100 тысяч.
 
 def is_simple(num: int) -> bool:
-    if num < 2 or num % 2 == 0:
-        return False
     if num == 2:
         return True
+    if num < 2 or num % 2 == 0:
+        return False
     limit = int(num ** 0.5) + 1
     for i in range(3, limit, 2):
         if not num % i:
@@ -18,7 +18,7 @@ def is_simple(num: int) -> bool:
 def main():
     while True:
         number = input('Введите целое число для проверки'
-                       '\nв диапазоне 0 - 100 000'
+                       '\nв диапазоне 2 - 100 000'
                        '\n(или exit для выхода):\n'
                        '_> ')
         if number.strip().lower() == 'exit':
@@ -27,21 +27,14 @@ def main():
             number = int(number)
         except ValueError:
             print('Требуется целое число.')
-        if number < 0 or number > 1e5:
-            print('Требуется число в диапазоне 0 - 100 000.')
+        if number < 2 or number > 1e5:      # единицу н относят к простым числам
+            print('Требуется число в диапазоне 2 - 100 000.')
         else:
             if is_simple(number):
-                print(f'{number} -- простое')
+                print(f'{number} -- простое\n')
             else:
-                print(f'{number} -- составное')
-
-
-def test():
-    for j in range(int(1e5)):
-        if is_simple(j):
-            print(j, "prime")
+                print(f'{number} -- составное\n')
 
 
 if __name__ == '__main__':
-    # main()
-    test()
+    main()
