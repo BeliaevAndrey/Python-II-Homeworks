6a67090bf727ad10f91f8be9d82aa0fa75ee2199
Andrei Beliaev <abbot190414@gmail.com>
2023-03-15 10:48:12 +0300

    HW03: task-03 completed

--- homework_03/task03.py
+++ homework_03/task03.py
@@ -1,4 +1,17 @@
 # 3. В большой текстовой строке подсчитать количество встречаемых слов и
 # вернуть 10 самых частых. Не учитывать знаки препинания и регистр символов.
 # За основу возьмите любую статью из википедии или из документации к языку.
+import re
 
+source = dir.__doc__
+
+pattern = re.compile("[\\[\\]\\(\\)<>_\",.?!;:-]")
+
+string = source.replace('\n', ' ').replace("'s", '').lower()
+string = ''.join(ch if ch not in pattern.findall(string) else ' ' for ch in string).split()
+
+frequency_sort2 = set(string)
+
+frequency_sort2 = sorted(frequency_sort2, key=lambda w: string.count(w), reverse=True)
+
+print('\n'.join(frequency_sort2[:10]))
