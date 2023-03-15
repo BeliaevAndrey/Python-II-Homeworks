034d2703415bdc2b38fb178b965dc74ff6b490e8
Andrei Beliaev <abbot190414@gmail.com>
2023-03-15 15:19:52 +0300

    HW03: task-04 table output added

--- homework_03/task04.py
+++ homework_03/task04.py
@@ -56,32 +56,61 @@ def prepare():
     return totals
 
 
-def start_packing(totals: dict) -> tuple[dict, int, list]:
+def start_packing(totals: dict) -> tuple[dict, int, dict]:
     total_weight = 0
     final_pack = {}
-    dropped_things = []
+    dropped_things = {key: [] for key in JOURNEY_STUFF.keys()}
     for i_thing in totals.items():
         total_weight += i_thing[1]
         final_pack.setdefault(*i_thing)
         while total_weight > BACKPACK_LC:
             dropped = final_pack.popitem()
             total_weight -= dropped[1]
-            dropped_things.append(dropped)
+            dropped_things.setdefault(search(dropped[0]), [])
+            dropped_things.get(search(dropped[0])).append(dropped)
+
     return final_pack, total_weight, dropped_things
 
 
-def print_results(final_pack: dict, total_weight: int, dropped_things: list) -> None:
-    print(f'{" Final weight: " + str(round(total_weight, 3)) + " ":*^50}')
-    print(f'{" Final pack: ":*^50}')
-    for i_thing, i_weight in sorted(final_pack.items()):
-        print(i_thing, i_weight)
-    print(f'{" Dropped: ":*^50}')
-    for thing in dropped_things:
-        print(*thing)
+def search(thing: str) -> str:
+    for i_key, i_value in JOURNEY_STUFF.items():
+        if thing in i_value.keys():
+            return i_key
+
+
+def form_table(final_pack: dict, total_weight: int, dropped_things: dict) -> str:
+
+    def table_assembly(in_dict: dict, out_list: list = None) -> list:
+        longest = len(max(table.values(), key=len))
+        if out_list is None:
+            out_list = []
+        for i in range(longest):
+            out_list.append(
+                [f' {in_dict.get(key)[i][0]:15}{in_dict.get(key)[i][1]:5} | '
+                 if i < len(in_dict.get(key)) else f' {"":20} | '
+                 for key in in_dict.keys()
+                 ])
+        return out_list
+
+    out_string = f'{" Backpack weight: " + str(round(total_weight, 3)) + " ":*^50}\n'
+    table = {key: [] for key in JOURNEY_STUFF.keys()}
+    for item in final_pack.items():
+        category = search(item[0])
+        table.setdefault(category, [])
+        table[category].append(item)
+    line = '|' + ''.join(f' {key:^20} | ' for key in table.keys()) + '\n|'
+
+    out_string += line + '-'*(len(line) - 5) + '|\n|'
+    rows = table_assembly(table)
+    rows.append([f'{" Dropped things: ":-^{len(line) - 5}}|'])
+    table_assembly(dropped_things, rows)
+
+    out_string += '\n|'.join((''.join(row) for row in rows))
+    return out_string
 
 
 def main():
-    print_results(*start_packing(prepare()))
+    print(form_table(*start_packing(prepare())))
 
 
 if __name__ == '__main__':
