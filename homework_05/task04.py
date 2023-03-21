2c64347d58a6ed486baaf8806bbdf507e7210865
Andrei Beliaev <abbot190414@gmail.com>
2023-03-22 00:56:04 +0300

    HW05: task04 -- complete

---
+++ homework_05/task04.py
@@ -0,0 +1,13 @@
+# Создайте функцию генератор чисел Фибоначчи (см. Википедию)
+def fibonacci_gen(limit_pos: int):
+    fib = []
+    yield from [(k := (0 if i == 0 else 1 if i < 3 else (fib[i-1] + fib[i-2])), fib.append(k))[0]
+                for i in range(0, limit_pos)]
+
+
+def main():
+    print(*fibonacci_gen(11))
+
+
+if __name__ == '__main__':
+    main()
