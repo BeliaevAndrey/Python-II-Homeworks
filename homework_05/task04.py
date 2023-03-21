b5f3c73e01587063c4c3593e28133c5f1ae0611b
Andrei Beliaev <abbot190414@gmail.com>
2023-03-22 01:16:17 +0300

    HW05: task04 -- type-hinting fixed

--- homework_05/task04.py
+++ homework_05/task04.py
@@ -1,5 +1,8 @@
 # Создайте функцию генератор чисел Фибоначчи (см. Википедию)
-def fibonacci_gen(limit_pos: int):
+from typing import Iterable
+
+
+def fibonacci_gen(limit_pos: int) -> Iterable:
     fib = []
     yield from ((k := (0 if i == 0 else 1 if i < 3 else (fib[i-1] + fib[i-2])), fib.append(k))[0]
                 for i in range(0, limit_pos))
