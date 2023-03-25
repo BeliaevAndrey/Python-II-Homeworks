dca18ab904ff2afa7d9006b086bea68d68f3ba20
Andrei Beliaev <abbot190414@gmail.com>
2023-03-25 13:51:45 +0300

    HW06: task02 solved

--- homework_06/sem06_modules/s06_date_check.py
+++ homework_06/sem06_modules/s06_date_check.py
@@ -1,12 +1,11 @@
-__all__ = ['checker']
+__all__ = ['checker', 'argv']
 
-from datetime import date
 from sys import argv
-# import re
 
 _LEAP_DIVISOR = 4
 _EXCLUDE = 100
 _INCLUDE = 400
+_MONTH_DAYS = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
 
 def _leap_year_check(a_year: int) -> bool:
@@ -15,6 +14,7 @@ def _leap_year_check(a_year: int) -> bool:
         return True
     if a_year % _LEAP_DIVISOR == 0:
         return a_year % _EXCLUDE != 0
+    return False
 
 
 def checker(date_in: str) -> bool:
@@ -25,27 +25,17 @@ def checker(date_in: str) -> bool:
     """
     if len(date_in := date_in.split('.')) != 3:
         return False
-    date_in = [*map(int, date_in)][::-1]
-    if date_in[1] > 31 or (date_in[1] == 2 and
-                           date_in[0] == 29 and
-                           not _leap_year_check(date_in[2])):
+    if not all(map(lambda x: x.isdigit(), date_in)):
         return False
-    try:
-        date(*date_in)
-    except ValueError:
+    date_in = [*map(int, date_in)]
+    if date_in[0] < 1 or date_in[1] > len(_MONTH_DAYS) or date_in[1] <= 0:
+        return False
+    if date_in[1] == 2 and _leap_year_check(date_in[2]) and date_in[0] <= 29:
+        return True
+    elif date_in[0] > _MONTH_DAYS[date_in[1] - 1]:
         return False
     return True
 
 
 if __name__ == '__main__':
     print("Not for separate use")
-    # pattern = re.compile("\\d{1,2}\\.\\d{1,2}\\.\\d{4}")
-    # if not argv:
-    #     print("Date not found")
-    # else:
-    #     for item in argv[1:]:
-    #         if pattern.match(item):
-    #             print("The date is right indeed" if checker(item) else "The date is NOT right indeed")
-    #             break
-    #     else:
-    #         print("Date not found")
--- homework_06/task02.py
+++ homework_06/task02.py
@@ -1,16 +1,22 @@
 from sem06_modules import s06_date_check
 import re
 
+PATTERN = re.compile("\\d{1,2}\\.\\d{1,2}\\.\\d{1,4}")
 
-pattern = re.compile("\\d{1,2}\\.\\d{1,2}\\.\\d{1,4}")
-if not s06_date_check.argv:
-    print("No arguments found")
-else:
-    for item in s06_date_check.argv[1:]:
-        if pattern.match(item):
-            print("The date is right indeed"
-                  if s06_date_check.checker(item)
-                  else "The date is NOT right indeed")
-            break
+
+def main():
+    if not s06_date_check.argv:
+        print("No arguments found")
     else:
-        print("Date not found")
+        for item in s06_date_check.argv[1:]:
+            if PATTERN.match(item):
+                print("The date is right indeed"
+                      if s06_date_check.checker(item)
+                      else "The date is NOT right indeed")
+                break
+        else:
+            print("Date not found")
+
+
+if __name__ == '__main__':
+    main()
