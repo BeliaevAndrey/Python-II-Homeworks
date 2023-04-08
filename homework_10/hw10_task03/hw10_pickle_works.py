b0096df63f55d5991953a1baad122fda68c4b7d9
Andrei Beliaev <abbot190414@gmail.com>
2023-04-08 10:30:13 +0300

    HW10: task 03 -- class PickleWorks added

--- homework_10/hw10_task03/hw10_pickle_works.py
+++ homework_10/hw10_task03/hw10_pickle_works.py
@@ -3,34 +3,38 @@ import os
 from datetime import datetime as dt
 
 
-def pickle_writer(in_dct: [list[dict], dict],
-                  path: str,
-                  file_name: str) -> None:
-    """
-     Writes to disk binary file of dictionary passed
-    :param in_dct: dict -- dictionary to write
-    :param path: str -- path where binary file to write
-    :param file_name: str -- file name WITHOUT extension (extension ".bin" used)
-    :return: None
-    """
-    file_path = os.path.join(path, file_name + '.bin')
-    if os.path.exists(file_path):
-        file_name += '_copy_' + str(dt.now()).replace(' ', '_').replace(':', '-').split('.')[0]
-        file_path = os.path.join(path, file_name + '.bin')
+class PickleWorks:
 
-    with open(file_path, 'wb') as f_out:
-        pickle.dump(in_dct, f_out)
+    @staticmethod
+    def pickle_writer(in_dct: [list[dict], dict],
+                      output_file_name: str,
+                      output_file_path: str = os.getcwd(),
+                      ) -> None:
+        """
+         Writes to disk binary file of dictionary passed
+        :param output_file_path:  str -- path where binary file to write
+        :param output_file_name:  -- file name
+        :param in_dct: dict -- dictionary to write
+        :return: None
+        """
+        file_path = os.path.join(output_file_path, output_file_name)
+        with open(file_path, 'wb') as f_out:
+            pickle.dump(in_dct, f_out)
 
-
-def pickle_reader(file_in_path: str) -> list[dict[str]]:
-    """
-    Reads binary file to a list of dictionaries
-    :param file_in_path: str -- path to a file to be read
-    :return: list[dict[str]]
-    """
-    with open(file_in_path, 'rb') as f_in:
-        out_lst = pickle.load(f_in, encoding='utf-8')
-    return out_lst
+    @staticmethod
+    def pickle_reader(output_file_name: str,
+                      output_file_path: str = os.getcwd(),
+                      ) -> list[dict[str]]:
+        """
+        Reads binary file to a list of dictionaries
+        :param output_file_path: str    -- path to a file to be read
+        :param output_file_name: str
+        :return: list[dict[str]]
+        """
+        file_path = os.path.join(output_file_path, output_file_name)
+        with open(file_path, 'rb') as f_in:
+            out_lst = pickle.load(f_in, encoding='utf-8')
+        return out_lst
 
 
 if __name__ == '__main__':
