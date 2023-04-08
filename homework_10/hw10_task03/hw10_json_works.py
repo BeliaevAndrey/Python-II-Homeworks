291ca8ac0d3d6b40dfd5b6d805736688817cee48
Andrei Beliaev <abbot190414@gmail.com>
2023-04-08 10:25:41 +0300

    HW10: task 03 -- class JsonWorks added

--- homework_10/hw10_task03/hw10_json_works.py
+++ homework_10/hw10_task03/hw10_json_works.py
@@ -1,36 +1,39 @@
 import json
 import os
-from datetime import datetime as dt
 
 
-def json_writer(in_dct: [list, dict],
-                path: str,
-                file_name: str) -> None:
-    """
-     Writes to disk json file of dictionary passed
-    :param in_dct: dict -- dictionary to write
-    :param path: str -- path where binary file to write
-    :param file_name: str -- file name WITHOUT extension (extension ".json" used)
-    :return: None
-    """
-    file_path = os.path.join(path, file_name + '.json')
-    if os.path.exists(file_path):
-        file_name += '_copy_' + str(dt.now()).replace(' ', '_').replace(':', '-').split('.')[0]
-        file_path = os.path.join(path, file_name + '.json')
+class JsonWorks:
 
-    with open(file_path, 'w', encoding='utf-8') as f_out:
-        json.dump(in_dct, f_out, indent=4)
+    @staticmethod
+    def json_writer(in_dct: [list, dict],
+                    output_file_name: str,
+                    output_file_path: str = os.getcwd(),
+                    ) -> None:
+        """
+         Writes to disk json file of dictionary passed
+        :param in_dct: dict -- dictionary to write
+        :param output_file_path: str -- path where binary file to write
+        :param output_file_name: str -- file name
+        :return: None
+        """
+        file_path = os.path.join(output_file_path, output_file_name)
+        with open(file_path, 'w', encoding='utf-8') as f_out:
+            json.dump(in_dct, f_out, indent=4)
 
-
-def json_reader(file_path: str) -> dict[str]:
-    """
-    Reads json file to a dictionary
-    :param file_path: str -- path to a file to be read
-    :return: dict[str] -- deserialized dictionary
-    """
-    with open(file_path, 'r', encoding='utf-8') as f_in:
-        json_dict = json.load(f_in)
-    return json_dict
+    @staticmethod
+    def json_reader(output_file_name: str,
+                    output_file_path: str = os.getcwd(),
+                    ) -> dict[str]:
+        """
+        Reads json file to a dictionary
+        :param output_file_name: str -- filename
+        :param output_file_path: str -- path to a file to be read
+        :return: dict[str] -- deserialized dictionary
+        """
+        file_path = os.path.join(output_file_path, output_file_name)
+        with open(file_path, 'r', encoding='utf-8') as f_in:
+            json_dict = json.load(f_in)
+        return json_dict
 
 
 if __name__ == '__main__':
