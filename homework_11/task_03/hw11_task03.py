0e8e5afa36829278461a00fb6d84b29292b09da4
Andrei Beliaev <abbot190414@gmail.com>
2023-04-12 00:33:07 +0300

    HW11: task 03 -- class Matrix added __mul__- and __rmul__-methods are added

--- homework_11/task_03/hw11_task03.py
+++ homework_11/task_03/hw11_task03.py
@@ -28,14 +28,62 @@ class Matrix:
         if not isinstance(other, self.__class__):
             raise TypeError("Not a 'Matrix'-type object")
         if self._rows != other._rows or self._cols != other._cols:
-            raise ValueError("Operation not supported for different-dimensional matrices")
+            raise ValueError("Operation not permitted for different-dimensional matrices")
         new_matrix = [[0 for _ in range(self._cols)] for _ in range(self._rows)]
         for j in range(self._rows):
             for i in range(self._cols):
                 new_matrix[j][i] = self._a_matrix[j][i] + other._a_matrix[j][i]
         return Matrix(new_matrix)
 
+    """
+       static int[][] mMultiply(int[][] matrixL, int[][] matrixR) {
+            int[][] rMatrix = new int[matrixL.length][matrixR[0].length];
+            for (int i = 0; i < matrixL.length; i++) {
+                for (int j = 0; j < matrixR[0].length; j++) {
+                    for (int k = 0; k < matrixR.length; k++) {
+                        rMatrix[i][j] += matrixL[i][k] * matrixR[k][j];
+                    }
+                }
+            }
+    
+    """
+
+    def __mul__(self, other) -> 'Matrix':
+        """
+        Calculates a multiplication of matrices or multiplication the matrix by a number
+        :param other: [int, float, Matrix]    -- other Matrix object
+        :return: Matrix                       -- new Matrix object
+        """
+        if isinstance(other, self.__class__):
+            return self.__rmul__(other)
+        elif isinstance(other, int) or isinstance(other, float):
+            new_matrix = [[0 for _ in range(self._cols)] for _ in range(self._rows)]
+            for j in range(self._rows):
+                for i in range(self._cols):
+                    new_matrix[j][i] = self._a_matrix[j][i] * other
+            return Matrix(new_matrix)
+        else:
+            raise TypeError("Unsupported operation")
+
+    def __rmul__(self, other) -> 'Matrix':
+        """
+        Calculates a multiplication of matrices
+        :param other: Matrix    -- other Matrix object
+        :return: Matrix         -- new Matrix object
+        """
+        if not isinstance(other, self.__class__):
+            raise TypeError("Not a 'Matrix'-type object")
+        if self._cols != other._rows:
+            raise ValueError("Operation not permitted if rows amount of first matrix "
+                             "is not equal to columns amount of other one")
+        new_matrix = [[0 for _ in range(other._rows)] for _ in range(self._rows)]
+        for j in range(self._rows):
+            for i in range(other._rows):
+                new_matrix[j][i] = self._a_matrix[j][i] * other._a_matrix[i][j]
+        return Matrix(new_matrix)
+
     def __eq__(self, other) -> bool:
+        """Returns True if the matrix equals to other one"""
         if self is other:
             return True
         if not isinstance(other, self.__class__):
@@ -70,6 +118,7 @@ def main():
     print(mtx_d)
 
     print(f'{mtx_a == mtx_b=}')
+    print(f'{mtx_a == mtx_b=}')
     print(f'{mtx_b == mtx_c=}')
     print(f'{mtx_b != mtx_c=}')
     print(f'{mtx_c != mtx_d=}')
@@ -78,7 +127,19 @@ def main():
     try:
         print(mtx_c + mtx_d)
     except ValueError as exc:
-        print(f'FAIL! =) {exc.__class__.__name__}: {exc}')
+        print(f'FAIL! {exc.__class__.__name__}: {exc}')
+    try:
+        print(mtx_a * mtx_b)
+    except ValueError as exc:
+        print(f'FAIL! {exc.__class__.__name__}: {exc}')
+    try:
+        print(mtx_a * mtx_d)
+    except ValueError as exc:
+        print(f'FAIL! {exc.__class__.__name__}: {exc}')
+    try:
+        print(mtx_a * 10)
+    except TypeError as exc:
+        print(f'FAIL! {exc.__class__.__name__}: {exc}')
 
 
 if __name__ == '__main__':
